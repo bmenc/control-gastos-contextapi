cfb3964708fb2f388b92d48b6ce72eb24c81155f
bmenc <bmenchaca.cca@gmail.com>
2025-12-30 20:03:26 -0600

    194. Creando el Reducer para Presupuestos

---
+++ src/reducers/budget-reducer.ts
@@ -0,0 +1,26 @@
+export type BudgetActions =
+    { type: 'add-budget', payload: { budget: number } }
+
+export type BudgetState = {
+    budget: number
+}
+
+export const initialState: BudgetState = {
+    budget: 0
+}
+
+export const budgetReducer = (
+    state: BudgetState = initialState,
+    actions: BudgetActions
+) => {
+    switch (actions.type) {
+        case 'add-budget': {
+            return {
+                ...budgetReducer,
+                budget: actions.payload.budget
+            }
+        }
+
+        default: return state;
+    }
+}
