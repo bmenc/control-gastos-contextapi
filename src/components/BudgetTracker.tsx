64410c117175b1c1644eca33d5bfb1c3880131ad
bmenc <bmenchaca.cca@gmail.com>
2026-01-02 13:57:59 -0600

    201. Creando el Componente con las cantidades y la gráfica

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,8 +1,31 @@
 import { useBudget } from "../hooks/useBudget";
+import AmountDisplay from "./AmountDisplay";
 
 export default function BudgetTracker() {
     const { state } = useBudget();
     return (
-        <>{state.budget}</>
+        <div className="grid grid-cols-1 md-grid-cols-2 gap-5">
+            <div className="flex justify-center">
+                <img src="/grafico.jpg" alt="Gráfica de gastos" />
+            </div>
+            <div className="flex flex-col justify-center items-center gap-8">
+                <button
+                    type="button"
+                    className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
+                >Resetear App</button>
+                <AmountDisplay
+                    label="Presupuesto"
+                    amount={100}
+                />
+                <AmountDisplay
+                    label="Disponible"
+                    amount={200}
+                />
+                <AmountDisplay
+                    label="Gastado"
+                    amount={300}
+                />
+            </div>
+        </div>
     )
 }
