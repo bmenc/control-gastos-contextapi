c6dd8baf3e237b6175356f46c08b115a0ef7d47a
bmenc <bmenchaca.cca@gmail.com>
2026-01-09 10:19:58 +0600

    202. Instalando Headless UI para tener una ventana modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useMemo } from "react";
 import BudgetForm from "./components/BudgetForm"
 import { useBudget } from "./hooks/useBudget";
 import BudgetTracker from "./components/BudgetTracker";
+import ExpenseModal from "./components/ExpenseModal";
 
 function App() {
   const { state } = useBudget();
@@ -12,10 +13,16 @@ function App() {
     <>
       <header className="bg-blue-600 py-8 max-h-87">
         <h1 className="uppercase text-center font-black text-4xl text-white">Planificador de Gastos</h1>
-        <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-          {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
-        </div>
       </header>
+
+      <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
+        {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
+      </div>
+      {isValidBudget && (
+        <main className="max-w-3xl mx-auto py-10">
+          <ExpenseModal />
+        </main>
+      )}
     </>
   )
 }
