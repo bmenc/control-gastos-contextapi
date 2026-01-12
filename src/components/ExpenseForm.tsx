b9293e5e96e49d7769ef0140ec01a9b5ec015799
bmenc <bmenchaca.cca@gmail.com>
2026-01-12 11:13:44 +0600

    208. Escribiendo el gasto en el State

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import type { DraftExpense } from "../types";
+import { useState, type ChangeEvent } from "react";
+import type { DraftExpense, Value } from "../types";
 import { categories } from "../data/categories";
 import DatePicker from 'react-date-picker';
 import 'react-calendar/dist/Calendar.css';
@@ -14,6 +14,21 @@ export default function ExpenseForm() {
   })
   // const [value, onChange] = useState<Value>(new Date());
 
+  const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    const isAmoutField = ['amount'].includes(name);
+    setExpense({
+      ...expense,
+      [name]: isAmoutField ? +value : value
+    })
+  }
+
+  const handleChangeDate = (value: Value) => {
+    setExpense({
+      ...expense,
+      date: value
+    });
+  }
 
   return (
     <form className="space-y-5">
@@ -31,6 +46,7 @@ export default function ExpenseForm() {
           className="bg-slate-100 p-2"
           name="expenseName"
           value={expense.expenseName}
+          onChange={handleChange}
         />
       </div>
 
@@ -46,6 +62,7 @@ export default function ExpenseForm() {
           className="bg-slate-100 p-2"
           name="amount"
           value={expense.amount}
+          onChange={handleChange}
         />
       </div>
 
@@ -59,6 +76,7 @@ export default function ExpenseForm() {
           className="bg-slate-100 p-2"
           name="category"
           value={expense.category}
+          onChange={handleChange}
         >
           <option value="">-- Seleccione --</option>
           {categories.map(category => (
@@ -78,6 +96,7 @@ export default function ExpenseForm() {
         <DatePicker
           className="bg-slate-100 p-2 border-0"
           value={expense.date}
+          onChange={handleChangeDate}
         />
       </div>
 
