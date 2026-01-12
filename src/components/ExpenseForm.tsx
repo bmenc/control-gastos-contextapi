5562e455920d14fdbe485d57b0269065fa2799c6
bmenc <bmenchaca.cca@gmail.com>
2026-01-12 08:47:06 -0600

    209. Mostrando una alerta si la validación falla en el formulario

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,9 +1,10 @@
-import { useState, type ChangeEvent } from "react";
+import { useState, type ChangeEvent, type FormEvent } from "react";
 import type { DraftExpense, Value } from "../types";
 import { categories } from "../data/categories";
 import DatePicker from 'react-date-picker';
 import 'react-calendar/dist/Calendar.css';
 import 'react-date-picker/dist/DatePicker.css';
+import ErrorMessage from "./ErrorMessage";
 
 export default function ExpenseForm() {
   const [expense, setExpense] = useState<DraftExpense>({
@@ -12,7 +13,7 @@ export default function ExpenseForm() {
     category: '',
     date: new Date()
   })
-  // const [value, onChange] = useState<Value>(new Date());
+  const [error, setError] = useState('');
 
   const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
     const { name, value } = e.target;
@@ -30,10 +31,21 @@ export default function ExpenseForm() {
     });
   }
 
-  return (
-    <form className="space-y-5">
-      <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">Nuevo Gasto</legend>
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
+    if (Object.values(expense).includes('')) {
+      setError('Todos los campos son obligatorios');
+      return;
+    }
+
+    setError('todo bien');
+  }
+
+  return (
+    <form className="space-y-5" onSubmit={handleSubmit}>
+      <legend className="uppercase text-center text-2xl font-black border-b-4 border-blue-500 py-2">Nuevo Gasto</legend>
+      {error && <ErrorMessage>{error}</ErrorMessage>}
       <div className="flex flex-col gap-2">
         <label
           htmlFor="expenseName"
