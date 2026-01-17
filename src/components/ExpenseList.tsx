cd1cabe6f520686f5401e2b583824c9a486f2aac
bmenc <bmenchaca.cca@gmail.com>
2026-01-17 15:27:50 -0600

    214. Mostrando los iconos de la categoría del gasto

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -8,7 +8,7 @@ export default function ExpenseList() {
   const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses]);
 
   return (
-    <div className="mt-10">
+    <div className="mt-10 space-y-2">
       {isEmpty ? <p className="text-gray-600 text-2xl font-bold">No hay Gastos</p> : (
         <>
           <p className="text-gray-600 text-2xl font-bold my-5"> Listado de Gastos.</p>
