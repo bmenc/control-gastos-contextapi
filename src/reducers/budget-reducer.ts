4864d2da1aabf8c59a2247d0405cf6dbf3d166a7
bmenc <57633529+bmenc@users.noreply.github.com>
2026-01-18 01:44:49 +0000

    215. Agregando interacción de Swipe a los gastos para Editar y Eliminar

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -6,9 +6,8 @@ export type BudgetActions =
   { type: 'add-budget', payload: { budget: number } } |
   { type: 'show-modal' } |
   { type: 'close-modal' } |
-  { type: 'add-expense', payload: { expense: DraftExpense } }
-
-
+  { type: 'add-expense', payload: { expense: DraftExpense } } |
+  { type: 'remove-expense', payload: { id: Expense['id'] } } 
 
 export type BudgetState = {
   budget: number,
