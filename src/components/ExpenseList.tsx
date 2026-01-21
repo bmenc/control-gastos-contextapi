7db8ed92621f789f29af5c3d660b43072798b4d0
bmenc <bmenchaca.cca@gmail.com>
2026-01-21 00:49:32 -0600

    228. Mostrar los gastos de la categoría seleccionada y Building de la App

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -7,12 +7,19 @@ export default function ExpenseList() {
   const { state } = useBudget();
   const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses]);
 
+  const filteredExpenses = useMemo(() => {
+    if (!state.currentCategoryId) {
+      return state.expenses;
+    }
+    return state.expenses.filter(expense => expense.category === state.currentCategoryId);
+  }, [state.expenses, state.currentCategoryId]);
+
   return (
     <div className="mt-10 bg-white rounded-lg p-10 shadow-lg">
       {isEmpty ? <p className="text-gray-600 text-2xl font-bold">No hay Gastos</p> : (
         <>
           <p className="text-gray-600 text-2xl font-bold my-5"> Listado de Gastos.</p>
-          {state.expenses.map(expense => (
+          {filteredExpenses.map(expense => (
             <ExpenseDetail
               key={expense.id}
               expense={expense}
